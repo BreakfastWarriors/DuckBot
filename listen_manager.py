8df7ea3afa904646f4b0f95b094ad49c728323fb
Xuanli Lin <kaz@kazumi.blog>
2023-02-21 12:36:57 -0700

    pin removal feature and tweaks to pin format

--- listen_manager.py
+++ listen_manager.py
@@ -1,8 +1,6 @@
-import datetime
 import json
 import os
 import sqlite3
-import pytz
 
 import discord.ext.commands as disc_cmds
 import discord.utils
@@ -16,6 +14,20 @@ class ListenerManager(disc_cmds.Cog, name='ListenerManager'):
         self.bot = bot
         self.daily_reminder = DailyReminder(self.bot)
         self.conn = sqlite3.connect('messages.sqlite')
+
+        # Pull the pin mapping json and load it
+        if os.path.isfile('data/pins.json'):
+            with open('data/pins.json') as pin_file:
+                self.pins = json.load(pin_file)
+        else:
+            self.pins = {}
+
+        self.pin_channel = discord.utils.get(self.bot.current_guild.text_channels, name=self.bot.pin_channel)
+        if not self.pin_channel:
+            print(f'Error: the pin channel {self.bot.pin_channel} does not exist')
+
+        self.duck_up = '<:duck_up:1071706220043452518>'
+        self.duck_down = '<:duck_down:1071706217845624842>'
 
     @disc_cmds.Cog.listener(name='on_ready')
     async def on_ready(self):
@@ -47,46 +59,43 @@ class ListenerManager(disc_cmds.Cog, name='ListenerManager'):
         await self.daily_reminder.remind(message)
         await quack_message(message)
 
+    # Bot pin feature
+    # Note: we have to use the "raw" version of it because the target message is not necessarily
+    # in the bot's message cache
     @disc_cmds.Cog.listener(name='on_raw_reaction_add')
     async def on_pin_reaction(self, reaction_event):
-        if reaction_event.emoji.name != '📌':
+        if reaction_event.emoji.name != '📌' or not self.pin_channel:
             return
 
-        # Get the destination channel
-        channel = discord.utils.get(self.bot.current_guild.text_channels, name=self.bot.pin_channel)
-        if not channel:
-            print(f'Error: the pin channel {channel} does not exist')
-            return
-
-        pin_msg = await self.bot.current_guild.get_channel(reaction_event.channel_id)\
+        target_msg = await self.bot.current_guild.get_channel(reaction_event.channel_id) \
             .fetch_message(reaction_event.message_id)
-        reaction = next((r for r in pin_msg.reactions if r.emoji == '📌'), None)
+        reaction = next((r for r in target_msg.reactions if r.emoji == '📌'), None)
         if not reaction:
             print(f'Warning: 📌 emoji not found among the reactions, message id: {reaction_event.message_id}')
 
         # Prevent the double-pinning
         # 1. message cannot be already pinned
         # 2. message cannot be on the pin channel
-        if reaction.count > 1 or pin_msg.channel == channel:
+        if reaction.count > 1 or target_msg.channel == self.pin_channel:
             return
 
         users = [user async for user in reaction.users()]
         pin_requester = users[0].id
 
-        msg_author = pin_msg.author.id
-        msg_content = pin_msg.content
+        msg_author = target_msg.author.id
+        msg_content = target_msg.content
         # Cap the message length at 600
         if len(msg_content) > 600:
             msg_content = msg_content[:600] + '...'
-        msg_channel = pin_msg.channel
-        msg_link = pin_msg.jump_url
-        msg_attachments = pin_msg.attachments
-        msg_id = pin_msg.id
-        msg_time = pin_msg.created_at
-        msg_embed = pin_msg.embeds
+        msg_channel = target_msg.channel
+        msg_link = target_msg.jump_url
+        msg_attachments = target_msg.attachments
+        msg_id = target_msg.id
+        msg_time = target_msg.created_at
+        msg_embed = target_msg.embeds
 
         # Assemble the pin message
-        content = f'<@{pin_requester}> just pinned a message from <#{msg_channel.id}>'
+        content = f'📌 by <@{pin_requester}> | <#{msg_channel.id}>'
         embed = discord.Embed()
         # embed.title = msg_author
         embed.description = f'<@{msg_author}>: {msg_content}'
@@ -100,12 +109,11 @@ class ListenerManager(disc_cmds.Cog, name='ListenerManager'):
             if 'video' in msg_attachments[0].content_type:
                 embed.description += f'\n`<{msg_attachments[0].filename}>`'
 
-        embeds = []
+        embeds = [embed]
 
         # Example timestring: 06/14/2022 6:03 PM MST
-        msg_time_mst = (msg_time - datetime.timedelta(hours=7)).strftime('%m/%d/%Y %I:%M %p MST')
-        embed.set_footer(text=f'Message ID {msg_id} · Posted {msg_time_mst}')
-        embeds.append(embed)
+        # msg_time_mst = (msg_time - datetime.timedelta(hours=7)).strftime('%m/%d/%Y %I:%M %p MST')
+        # embed.set_footer(text=f'Message ID {msg_id} · Posted {msg_time_mst}')
 
         if msg_embed:
             embeds.append(msg_embed[0])
@@ -113,5 +121,34 @@ class ListenerManager(disc_cmds.Cog, name='ListenerManager'):
         embed.description += f'\n\n**[Jump to the message]({msg_link})**'
 
         # Mention the users but do not actually ping them
-        await channel.send(content=content, embeds=embeds, allowed_mentions=discord.AllowedMentions.none())
+        pin_msg = await self.pin_channel.send(content=content,
+                                              embeds=embeds,
+                                              allowed_mentions=discord.AllowedMentions.none())
 
+        # Add reactions to the pin message
+        await pin_msg.add_reaction(self.duck_up)
+        await pin_msg.add_reaction(self.duck_down)
+
+        # Add the mapping between the target msg and the pin msg
+        self.pins[target_msg.id] = pin_msg.id
+        with open('data/pins.json', 'w') as outfile:
+            json.dump(self.pins, outfile)
+
+    # Pin removal
+    @disc_cmds.Cog.listener(name='on_raw_reaction_clear_emoji')
+    async def on_pin_reaction(self, reaction_event):
+        if reaction_event.emoji.name != '📌' or not self.pin_channel:
+            return
+
+        if reaction_event.message_id in self.pins:
+            pin_msg_id = self.pins[reaction_event.message_id]
+            pin_msg = await self.pin_channel.fetch_message(pin_msg_id)
+
+            if not pin_msg:
+                print(f'Error: pin message {pin_msg_id} not found')
+                return
+
+            await pin_msg.delete()
+            del self.pins[reaction_event.message_id]
+            with open('data/pins.json', 'w') as outfile:
+                json.dump(self.pins, outfile)
